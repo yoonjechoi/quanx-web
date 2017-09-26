e863965d3a769ceacca9480d52d32db8ad3289e2
Yoonje Choi <yoonjechoi@gmail.com>
2017-09-26 10:43:00 +0900

    chore(eslant): set eslant for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
+'use strict';
+
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "import"
-  ],
+  "extends": ["airbnb"],
+
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+
+  "plugins": [ "react", "import" ],
+
   "env": {
     "browser": true
-  }
+  },
+
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  },
+
 };
