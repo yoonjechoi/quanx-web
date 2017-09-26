1add3dfd5b699ad820cd8be3dddd4fdb3ae96626
Yoonje Choi <yoonjechoi@gmail.com>
2017-09-26 13:34:02 +0900

    feat(app): update movie data at componentDidMount

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -3,31 +3,29 @@ import PropTypes from 'prop-types';
 
 import './Movie.css';
 
-
-class Movie extends Component {
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  };
-  
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h3>{this.props.title}</h3>
-      </div>
-    );
-  }
+function Movie({title, poster}) {
+  return (
+    <div>
+      <MoviePoster poster={poster}/>
+      <h3>{title}</h3>
+    </div>
+  );
 }
 
-
-class MoviePoster extends Component {
-  render() {
-    return (
-      <img src={this.props.poster} alt="poster"/>
-    );
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
+
+function MoviePoster({poster}) {
+  return (
+    <img src={poster} alt="poster"/>
+  )
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+};
+
 
 export default Movie;
--- src/app.jsx
+++ src/app.jsx
@@ -5,35 +5,71 @@ import './app.css';
 import Movie from './Movie.jsx'
 
 
-const movies = [
-  {
-    title: 'Matrix',
-    poster: 'https://www.movieposter.com/posters/archive/main/9/A70-4902'
-  },
-  {
-    title: 'Full Metal Jacket',
-    poster: 'https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg'
-  },
-  {
-    title: 'Old boy',
-    poster: 'http://imgmovie.naver.com/mdi/mi/0369/C6944-04.jpg'
-  },
-  {
-    title: 'Star wars',
-    poster: 'https://i.pinimg.com/originals/27/11/4e/27114ed141e95e152a83ee17ad9014ad.jpg'
-  },
-];
-
-
 class App extends Component {
+
+  state = {}
+
   render() {
+
+    console.log('render')
+
     return (
       <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    });
+
+    console.log(movies);
+    return movies;
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: 'Matrix',
+            poster: 'https://www.movieposter.com/posters/archive/main/9/A70-4902'
+          },
+          {
+            title: 'Full Metal Jacket',
+            poster: 'https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg'
+          },
+          {
+            title: 'Old boy',
+            poster: 'http://imgmovie.naver.com/mdi/mi/0369/C6944-04.jpg'
+          },
+          {
+            title: 'Star wars',
+            poster: 'https://i.pinimg.com/originals/27/11/4e/27114ed141e95e152a83ee17ad9014ad.jpg'
+          },
+          {
+            title: 'Transpotting',
+            poster: 'https://www.bleedingcool.com/wp-content/uploads/2017/02/trainspotting-poster.jpg'
+          },
+        ]
+      })
+    }, 3000);
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate')
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
   }
 }
 
